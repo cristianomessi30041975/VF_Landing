40ffa70130f56ad22208620bef0acf0970d42fbb
minhhien22 <cristianomessi30041975@gmail.com>
2024-08-03 15:42:06 +0700

    update

---
+++ cdn-cgi/challenge-platform/h/b/scripts/jsd/8c81cb09042c/main.js
@@ -0,0 +1,199 @@
+window._cf_chl_opt = {
+    cFPWv: 'b'
+};
+~ function(V, g, h, m, n, x, y, A) {
+    V = b,
+        function(c, e, U, f, C) {
+            for (U = b, f = c(); !![];) try {
+                if (C = -parseInt(U(508)) / 1 + parseInt(U(419)) / 2 + parseInt(U(453)) / 3 + -parseInt(U(445)) / 4 * (-parseInt(U(470)) / 5) + -parseInt(U(461)) / 6 * (parseInt(U(483)) / 7) + -parseInt(U(491)) / 8 * (-parseInt(U(479)) / 9) + -parseInt(U(495)) / 10, e === C) break;
+                else f.push(f.shift())
+            } catch (D) {
+                f.push(f.shift())
+            }
+        }(a, 479554), g = this || self, h = g[V(414)], m = {}, m[V(484)] = 'o', m[V(428)] = 's', m[V(410)] = 'u', m[V(412)] = 'z', m[V(450)] = 'n', m[V(482)] = 'I', n = m, g[V(462)] = function(C, D, E, F, a4, H, I, J, K, L, M) {
+            if (a4 = V, D === null || void 0 === D) return F;
+            for (H = v(D), C[a4(488)][a4(498)] && (H = H[a4(471)](C[a4(488)][a4(498)](D))), H = C[a4(421)][a4(494)] && C[a4(489)] ? C[a4(421)][a4(494)](new C[(a4(489))](H)) : function(N, a5, O) {
+                    for (a5 = a4, N[a5(423)](), O = 0; O < N[a5(496)]; N[O] === N[O + 1] ? N[a5(487)](O + 1, 1) : O += 1);
+                    return N
+                }(H), I = 'nAsAaAb'.split('A'), I = I[a4(467)][a4(503)](I), J = 0; J < H[a4(496)]; K = H[J], L = s(C, D, K), I(L) ? (M = L === 's' && !C[a4(476)](D[K]), a4(437) === E + K ? G(E + K, L) : M || G(E + K, D[K])) : G(E + K, L), J++);
+            return F;
+
+            function G(N, O, a3) {
+                a3 = b, Object[a3(418)][a3(431)][a3(444)](F, O) || (F[O] = []), F[O][a3(425)](N)
+            }
+        }, x = V(493)[V(443)](';'), y = x[V(467)][V(503)](x), g[V(502)] = function(C, D, a6, E, F, G, H) {
+            for (a6 = V, E = Object[a6(474)](D), F = 0; F < E[a6(496)]; F++)
+                if (G = E[F], 'f' === G && (G = 'N'), C[G]) {
+                    for (H = 0; H < D[E[F]][a6(496)]; - 1 === C[G][a6(439)](D[E[F]][H]) && (y(D[E[F]][H]) || C[G][a6(425)]('o.' + D[E[F]][H])), H++);
+                } else C[G] = D[E[F]][a6(458)](function(I) {
+                    return 'o.' + I
+                })
+        }, A = function(a8, e, f, C) {
+            return a8 = V, e = String[a8(427)], f = {
+                'h': function(D) {
+                    return D == null ? '' : f.g(D, 6, function(E, a9) {
+                        return a9 = b, a9(497)[a9(449)](E)
+                    })
+                },
+                'g': function(D, E, F, aa, G, H, I, J, K, L, M, N, O, P, Q, R, S, T) {
+                    if (aa = a8, null == D) return '';
+                    for (H = {}, I = {}, J = '', K = 2, L = 3, M = 2, N = [], O = 0, P = 0, Q = 0; Q < D[aa(496)]; Q += 1)
+                        if (R = D[aa(449)](Q), Object[aa(418)][aa(431)][aa(444)](H, R) || (H[R] = L++, I[R] = !0), S = J + R, Object[aa(418)][aa(431)][aa(444)](H, S)) J = S;
+                        else {
+                            if (Object[aa(418)][aa(431)][aa(444)](I, J)) {
+                                if (256 > J[aa(416)](0)) {
+                                    for (G = 0; G < M; O <<= 1, P == E - 1 ? (P = 0, N[aa(425)](F(O)), O = 0) : P++, G++);
+                                    for (T = J[aa(416)](0), G = 0; 8 > G; O = 1 & T | O << 1.22, E - 1 == P ? (P = 0, N[aa(425)](F(O)), O = 0) : P++, T >>= 1, G++);
+                                } else {
+                                    for (T = 1, G = 0; G < M; O = O << 1 | T, P == E - 1 ? (P = 0, N[aa(425)](F(O)), O = 0) : P++, T = 0, G++);
+                                    for (T = J[aa(416)](0), G = 0; 16 > G; O = O << 1 | T & 1.7, P == E - 1 ? (P = 0, N[aa(425)](F(O)), O = 0) : P++, T >>= 1, G++);
+                                }
+                                K--, 0 == K && (K = Math[aa(463)](2, M), M++), delete I[J]
+                            } else
+                                for (T = H[J], G = 0; G < M; O = O << 1.97 | T & 1.87, E - 1 == P ? (P = 0, N[aa(425)](F(O)), O = 0) : P++, T >>= 1, G++);
+                            J = (K--, 0 == K && (K = Math[aa(463)](2, M), M++), H[S] = L++, String(R))
+                        }
+                    if ('' !== J) {
+                        if (Object[aa(418)][aa(431)][aa(444)](I, J)) {
+                            if (256 > J[aa(416)](0)) {
+                                for (G = 0; G < M; O <<= 1, E - 1 == P ? (P = 0, N[aa(425)](F(O)), O = 0) : P++, G++);
+                                for (T = J[aa(416)](0), G = 0; 8 > G; O = T & 1.21 | O << 1, P == E - 1 ? (P = 0, N[aa(425)](F(O)), O = 0) : P++, T >>= 1, G++);
+                            } else {
+                                for (T = 1, G = 0; G < M; O = O << 1 | T, E - 1 == P ? (P = 0, N[aa(425)](F(O)), O = 0) : P++, T = 0, G++);
+                                for (T = J[aa(416)](0), G = 0; 16 > G; O = O << 1 | T & 1.77, E - 1 == P ? (P = 0, N[aa(425)](F(O)), O = 0) : P++, T >>= 1, G++);
+                            }
+                            K--, K == 0 && (K = Math[aa(463)](2, M), M++), delete I[J]
+                        } else
+                            for (T = H[J], G = 0; G < M; O = O << 1.97 | T & 1, E - 1 == P ? (P = 0, N[aa(425)](F(O)), O = 0) : P++, T >>= 1, G++);
+                        K--, K == 0 && M++
+                    }
+                    for (T = 2, G = 0; G < M; O = T & 1 | O << 1, E - 1 == P ? (P = 0, N[aa(425)](F(O)), O = 0) : P++, T >>= 1, G++);
+                    for (;;)
+                        if (O <<= 1, E - 1 == P) {
+                            N[aa(425)](F(O));
+                            break
+                        } else P++;
+                    return N[aa(417)]('')
+                },
+                'j': function(D, ab) {
+                    return ab = a8, null == D ? '' : '' == D ? null : f.i(D[ab(496)], 32768, function(E, ac) {
+                        return ac = ab, D[ac(416)](E)
+                    })
+                },
+                'i': function(D, E, F, ad, G, H, I, J, K, L, M, N, O, P, Q, R, T, S) {
+                    for (ad = a8, G = [], H = 4, I = 4, J = 3, K = [], N = F(0), O = E, P = 1, L = 0; 3 > L; G[L] = L, L += 1);
+                    for (Q = 0, R = Math[ad(463)](2, 2), M = 1; R != M; S = N & O, O >>= 1, 0 == O && (O = E, N = F(P++)), Q |= M * (0 < S ? 1 : 0), M <<= 1);
+                    switch (Q) {
+                        case 0:
+                            for (Q = 0, R = Math[ad(463)](2, 8), M = 1; M != R; S = N & O, O >>= 1, O == 0 && (O = E, N = F(P++)), Q |= M * (0 < S ? 1 : 0), M <<= 1);
+                            T = e(Q);
+                            break;
+                        case 1:
+                            for (Q = 0, R = Math[ad(463)](2, 16), M = 1; M != R; S = N & O, O >>= 1, 0 == O && (O = E, N = F(P++)), Q |= (0 < S ? 1 : 0) * M, M <<= 1);
+                            T = e(Q);
+                            break;
+                        case 2:
+                            return ''
+                    }
+                    for (L = G[3] = T, K[ad(425)](T);;) {
+                        if (P > D) return '';
+                        for (Q = 0, R = Math[ad(463)](2, J), M = 1; R != M; S = N & O, O >>= 1, O == 0 && (O = E, N = F(P++)), Q |= M * (0 < S ? 1 : 0), M <<= 1);
+                        switch (T = Q) {
+                            case 0:
+                                for (Q = 0, R = Math[ad(463)](2, 8), M = 1; M != R; S = N & O, O >>= 1, 0 == O && (O = E, N = F(P++)), Q |= M * (0 < S ? 1 : 0), M <<= 1);
+                                G[I++] = e(Q), T = I - 1, H--;
+                                break;
+                            case 1:
+                                for (Q = 0, R = Math[ad(463)](2, 16), M = 1; R != M; S = O & N, O >>= 1, 0 == O && (O = E, N = F(P++)), Q |= M * (0 < S ? 1 : 0), M <<= 1);
+                                G[I++] = e(Q), T = I - 1, H--;
+                                break;
+                            case 2:
+                                return K[ad(417)]('')
+                        }
+                        if (0 == H && (H = Math[ad(463)](2, J), J++), G[T]) T = G[T];
+                        else if (I === T) T = L + L[ad(449)](0);
+                        else return null;
+                        K[ad(425)](T), G[I++] = L + T[ad(449)](0), H--, L = T, 0 == H && (H = Math[ad(463)](2, J), J++)
+                    }
+                }
+            }, C = {}, C[a8(429)] = f.h, C
+        }(), B();
+
+    function o(c, e, a0) {
+        return a0 = V, e instanceof c[a0(500)] && 0 < c[a0(500)][a0(418)][a0(511)][a0(444)](e)[a0(439)](a0(481))
+    }
+
+    function a(ah) {
+        return ah = 'getPrototypeOf,iframe,includes,loading,error on cf_chl_props,25UtnoMm,concat,jsd,msg,keys,catch,isNaN,style,isArray,143874ttDdgu,cFPWv,[native code],bigint,196483cUaacy,object,Message: ,random,splice,Object,Set,/invisible/jsd,448SZxJPu,clientInformation,_cf_chl_opt;TclWT0;TltI2;qeSZC2;LqTgj1;UPatB2;lZKwZ7;tUWJ2;UYbeL2;RaMt3;LfBy5;OnYIY1;xnCn5;CcuM6;DPHb1;xDKZ3,from,2311970ZZtOBL,length,c-2K9q1gIbTWGyJXYLixeO5Pajh8k7MrtHEuNfS+Qd$6zlwFAn30pCmVBUZv4oRsD,getOwnPropertyNames,send,Function,POST,DPHb1,bind,%2b,contentWindow,XMLHttpRequest,0.3955968239318957:1722439520:z94rXQzQh5ajTavSWOs-QeMeCl-ieN-ekV0vPDe6xmE,950190NImhXt,contentDocument,_cf_chl_opt,toString,replace,stringify,undefined,function,symbol,__CF$cv$params,document,ontimeout,charCodeAt,join,prototype,356638JFsoFP,/cdn-cgi/challenge-platform/h/,Array,application/x-www-form-urlencoded,sort,display: none,push, - ,fromCharCode,string,vAWKPtwK,now,hasOwnProperty,createElement,tabIndex,addEventListener,removeChild,timeout,d.cookie,readyState,indexOf,Content-Type,Content-type,Error object: ,split,call,418260ekxUvC,/0.3955968239318957:1722439520:z94rXQzQh5ajTavSWOs-QeMeCl-ieN-ekV0vPDe6xmE/,application/json,appendChild,charAt,number,floor,navigator,1456848CltbHn,DOMContentLoaded,body,open,onreadystatechange,map,/beacon/ov,/jsd/r/,90MsRvLk,CcuM6,pow,setRequestHeader'.split(','), a = function() {
+            return ah
+        }, a()
+    }
+
+    function i(c, W) {
+        return W = V, Math[W(486)]() < c
+    }
+
+    function v(c, a2, e) {
+        for (a2 = V, e = []; c !== null; e = e[a2(471)](Object[a2(474)](c)), c = Object[a2(465)](c));
+        return e
+    }
+
+    function s(e, C, D, a1, E) {
+        a1 = V;
+        try {
+            return C[D][a1(475)](function() {}), 'p'
+        } catch (F) {}
+        try {
+            if (null == C[D]) return void 0 === C[D] ? 'u' : 'x'
+        } catch (G) {
+            return 'i'
+        }
+        return e[a1(421)][a1(478)](C[D]) ? 'a' : C[D] === e[a1(421)] ? 'C' : !0 === C[D] ? 'T' : C[D] === !1 ? 'F' : (E = typeof C[D], a1(411) == E ? o(e, C[D]) ? 'N' : 'f' : n[E] || '?')
+    }
+
+    function l(f, C, Z, D, E, F, G, H, I, J) {
+        if (Z = V, !i(.01)) return ![];
+        D = [Z(485) + f, Z(442) + JSON[Z(513)](C)][Z(417)](Z(426));
+        try {
+            if (E = g[Z(413)], F = Z(420) + g[Z(510)][Z(480)] + Z(459) + 1 + Z(446) + E.r + Z(490), G = new g[(Z(506))](), !G) return;
+            H = Z(501), G[Z(456)](H, F, !![]), G[Z(436)] = 2500, G[Z(415)] = function() {}, G[Z(464)](Z(441), Z(422)), I = {}, I[Z(473)] = D, J = A[Z(429)](JSON[Z(513)](I))[Z(512)]('+', Z(504)), G[Z(499)]('v_' + E.r + '=' + J)
+        } catch (K) {}
+    }
+
+    function k(c, e, Y, f, C) {
+        Y = V, f = {
+            'wp': A[Y(429)](JSON[Y(513)](e)),
+            's': Y(507)
+        }, C = new XMLHttpRequest(), C[Y(456)](Y(501), Y(420) + g[Y(510)][Y(480)] + Y(460) + c), C[Y(464)](Y(440), Y(447)), C[Y(499)](JSON[Y(513)](f))
+    }
+
+    function z(a7, C, D, E, F, G) {
+        a7 = V;
+        try {
+            return C = h[a7(432)](a7(466)), C[a7(477)] = a7(424), C[a7(433)] = '-1', h[a7(455)][a7(448)](C), D = C[a7(505)], E = {}, E = CcuM6(D, D, '', E), E = CcuM6(D, D[a7(492)] || D[a7(452)], 'n.', E), E = CcuM6(D, C[a7(509)], 'd.', E), h[a7(455)][a7(435)](C), F = {}, F.r = E, F.e = null, F
+        } catch (H) {
+            return G = {}, G.r = {}, G.e = H, G
+        }
+    }
+
+    function B(ae, c, e, f, C) {
+        if (ae = V, c = g[ae(413)], !c) return;
+        if (!j()) return;
+        (e = ![], f = function(af, D) {
+            (af = ae, !e) && (e = !![], D = z(), k(c.r, D.r), D.e && l(af(469), D.e, af(472)))
+        }, h[ae(438)] !== ae(468)) ? f(): g[ae(434)] ? h[ae(434)](ae(454), f) : (C = h[ae(457)] || function() {}, h[ae(457)] = function(ag) {
+            ag = ae, C(), h[ag(438)] !== ag(468) && (h[ag(457)] = C, f())
+        })
+    }
+
+    function j(X, c, e, f, C) {
+        if ((X = V, c = g[X(413)], e = 3600, c.t) && (f = Math[X(451)](+atob(c.t)), C = Math[X(451)](Date[X(430)]() / 1e3), C - f > e)) return ![];
+        return !![]
+    }
+
+    function b(c, d, e) {
+        return e = a(), b = function(f, g, h) {
+            return f = f - 410, h = e[f], h
+        }, b(c, d)
+    }
+}()
